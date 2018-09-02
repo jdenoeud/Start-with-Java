6dfa336fd8c8023c0bab52cd9a5651e183e74119
Justine <justine.denoeud@gmail.com>
2018-09-02 17:27:57 +0200

    Add Customer, Product and Bill classes

---
+++ src/com/ocr/justine/Customer.java
@@ -0,0 +1,14 @@
+package com.ocr.justine;
+
+public class Customer {
+    String fullname;
+    String address;
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
