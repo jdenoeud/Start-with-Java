e218a1f0668a9bde466fad1f5c6e4117285430f8
Justine <justine.denoeud@gmail.com>
2018-09-07 13:10:15 +0200

    Add method displayBill

--- src/main/java/com/ocr/justine/web/BillServlet.java
+++ src/main/java/com/ocr/justine/web/BillServlet.java
@@ -1,16 +1,17 @@
 package com.ocr.justine.web;
 
-import com.ocr.justine.Fridge;
-import com.ocr.justine.Product;
-import com.ocr.justine.Television;
+import com.ocr.justine.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BillServlet extends HttpServlet {
     List<Product> products = new ArrayList<Product>();
@@ -31,10 +32,53 @@ public class BillServlet extends HttpServlet {
         resp.setContentType("texte.html");
         if (req.getQueryString() == null)
             displayForm(resp);
-
-
+        else
+            displayBill(req, resp);
     }
 
+    private void displayBill(HttpServletRequest req, HttpServletResponse resp) {
+        Map<String, String> params = splitParameters(req.getQueryString());
+        Customer customer = new Customer(params.get("fullname"), params.get("address"));
+        Delivery delivery = null;
+        switch (params.get("deliveryMode")) {
+            case "direct" :
+                delivery = new DirectDelivery();
+                break;
+            case "express" :
+                delivery = new ExpressDelivery(params.get("deliveryInfo"));
+                break;
+            case "relay" :
+                delivery = new RelayDelivery(Integer.parseInt(params.get("deliveryInfo")));
+                break;
+            case "takeAway" :
+                delivery = new TakeAwayDelivery();
+                break;
+        }
+        Bill bill = new Bill(customer, delivery);
+        String[] productsParams = params.get("products").split("%0D%0A");
+        for (String productLine : productsParams) {
+            String[] productAndQuantity = productLine.split("%3A");
+            Product product = products.get(Integer.parseInt(productAndQuantity[0]));
+            Integer quantity = Integer.parseInt(productAndQuantity[1]);
+            bill.addProduct(product, quantity);
+        }
+        bill.generate(new Writer() {
+            @Override
+            public void start() {
+            }
+            @Override
+            public void writeLine(String line) {
+                try {
+                    resp.getWriter().println("<br/>" + line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void stop() {
+            }
+        });
+    }
     private void displayForm(HttpServletResponse resp) throws IOException {
         for (int i = 0; i < products.size(); i++) {
             Product product = products.get(i);
@@ -57,4 +101,17 @@ public class BillServlet extends HttpServlet {
                 "</form>";
         resp.getWriter().println(form);
     }
+
+
+    public Map<String, String> splitParameters(String queryString) {
+        String[] brutParams = queryString.split("&");
+        Map<String, String> params = new HashMap<>();
+        for (String brutParam : brutParams) {
+            String[] keyAndValue = brutParam.split("=");
+            if (keyAndValue.length == 2)
+                params.put(keyAndValue[0], keyAndValue[1]);
+        }
+        return params;
+    }
+
 }
---
+++ test/com/ocr/justine/BillServletTest.java
@@ -0,0 +1,39 @@
+package com.ocr.justine;
+
+import com.ocr.justine.web.BillServlet;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BillServletTest {
+
+    @Test
+    public void Given_TwoPArameters_When_SplitParams_Then_fillMapCorrectly(){
+        BillServlet billServlet = new BillServlet();
+        Map<String, String> params = billServlet.splitParameters("p1=v1&p2=v2");
+        assertEquals(2, params.size());
+        assertEquals("v1", params.get("p1"));
+        assertEquals("v2", params.get("p2"));
+    }
+
+    @Test
+    public void Given_OneEmptyParamsAndOneGoodParams_When_splitParans_Then_fillMapCorrectly() {
+        BillServlet billServlet = new BillServlet();
+        Map<String, String> params = billServlet.splitParameters("p1=&p2=v2");
+        assertEquals(1, params.size());
+        assertEquals("", params.get("p1"));
+        assertEquals("v2", params.get("p2"));
+    }
+
+    @Test
+    public void Given_NoParams_When_splitParans_Then_fillMapCorrectly() {
+        BillServlet billServlet = new BillServlet();
+        Map<String, String> params = billServlet.splitParameters("test");
+        assertEquals(0, params.size());
+
+    }
+
+
+}
